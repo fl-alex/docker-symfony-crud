d34b935812994d085a0f54a697ed543db3156902
Oleksandr Bondarenko <o.bondarenko@esprzedaz.com>
2023-05-31 23:46:48 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route(path: '/', name: 'articles', methods: ['GET'])]
+    public function list(): Response
+    {
+        return new Response('Welcome to Latte and Code++----+ ');
+    }
+}

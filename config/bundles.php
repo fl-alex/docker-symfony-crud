5ae2e4e3670a53c287443ad0c6f17f9ab476ac6a
Oleksandr Bondarenko <o.bondarenko@esprzedaz.com>
2023-06-09 12:44:41 +0200

    Remove webpack-encore-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
